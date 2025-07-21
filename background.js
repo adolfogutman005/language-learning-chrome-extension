e7474256b498c341907496b350f1f9676010d7fa
Adolf <adolf@starship.localdomain>
2025-07-21 14:08:24 -0400

    first commit

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "language-helper",
+    title: "Check Language Info",
+    contexts: ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "language-helper") {
+    chrome.storage.local.set({ selectedText: info.selectionText }, () => {
+      chrome.action.openPopup();
+    });
+  }
+});
